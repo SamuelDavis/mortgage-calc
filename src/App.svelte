c332cb741494fea9abe2fe62195fb727efc15577
Samuel Davis <samueljakdavis@gmail.com>
2023-04-22 21:58:04 -0400

    add totals summary

--- src/App.svelte
+++ src/App.svelte
@@ -5,42 +5,59 @@
   const downPayment = writable(0.2);
   const principal = derived(
     [purchasePrice, downPayment],
-    ([purchasePrice, downPayment]) => {
-      return purchasePrice - purchasePrice * downPayment;
-    }
+    ([purchasePrice, downPayment]) =>
+      downPayment > 1
+        ? purchasePrice - downPayment
+        : purchasePrice * downPayment
   );
-  const monthlyPayment = writable(1500);
   const annualInterestRate = writable(0.065);
   const monthlyInterestRate = derived(
     [annualInterestRate],
     ([value]) => value / 12
   );
+  const monthlyPayment = writable(2000);
   const amortization = derived(
-    [principal, monthlyPayment, monthlyInterestRate],
-    ([principal, monthlyPayment, monthlyInterestRate]) => {
+    [principal, monthlyInterestRate, monthlyPayment],
+    ([principal, monthlyInterestRate, monthlyPayment]) => {
+      monthlyPayment = Math.max(
+        principal * monthlyInterestRate + 1,
+        monthlyPayment
+      );
+
       let schedule: [number, number, number, number][] = [];
-      let totalInterest = 0;
-      let totalPrincipal = 0;
+      let interestTotal = 0;
+      let principalTotal = 0;
       while (principal > 0) {
         const interestPayment = principal * monthlyInterestRate;
         const principalPayment = Math.min(
           principal,
           monthlyPayment - interestPayment
         );
-        if (principalPayment <= 0) return [];
         principal -= principalPayment;
-        totalInterest += interestPayment;
-        totalPrincipal += principalPayment;
+        interestTotal += interestPayment;
+        principalTotal += principalPayment;
         schedule.push([
           interestPayment,
-          totalInterest,
+          interestTotal,
           principalPayment,
-          totalPrincipal,
+          principalTotal,
         ]);
       }
+
       return schedule;
     }
   );
+  const totals = derived([amortization], ([amortization]) => {
+    const [, interest] = amortization[amortization.length - 1] ?? [
+      undefined,
+      0,
+    ];
+    return {
+      months: amortization.length + 1,
+      years: Math.ceil((amortization.length + 1) / 12),
+      interest,
+    };
+  });
 
   function formatNumber(number: number) {
     const [value, fraction] = number
@@ -54,30 +71,66 @@
 <main>
   <form on:submit|preventDefault>
     <label>
-      Purchase Price $<input
-        type="number"
-        step="0.01"
+      Purchase Price <input
         bind:value={$purchasePrice}
+        class="dollar"
+        min="0"
+        step="0.01"
+        type="number"
       />
     </label>
     <label>
-      Down Payment %<input type="number" step="1" bind:value={$downPayment} />
+      Down Payment <input
+        bind:value={$downPayment}
+        class="percent"
+        type="number"
+      />
     </label>
     <label>
-      Monthly Payment $<input
-        type="number"
-        step="0.01"
+      Monthly Payment <input
         bind:value={$monthlyPayment}
-        min={$annualInterestRate * $principal}
+        class="dollar"
+        min={$monthlyInterestRate * $principal}
+        step="50"
+        type="number"
       />
     </label>
     <label>
-      Annual Interest Rate %<input
-        type="number"
-        step="0.01"
+      Annual Interest Rate <input
         bind:value={$annualInterestRate}
+        class="percent"
+        min="0"
+        step="0.01"
+        type="number"
       />
     </label>
+    <fieldset>
+      <legend>Summary</legend>
+      <label>
+        Down Payment
+        {#if $downPayment < 1}
+          $
+          <output>
+            {formatNumber($purchasePrice * $downPayment)}
+          </output>
+        {:else}
+          %
+          <output>{formatNumber(($downPayment / $purchasePrice) * 100)}</output>
+        {/if}
+      </label>
+      <label>
+        Months
+        <output>{$totals.months}</output>
+      </label>
+      <label>
+        Years
+        <output>{$totals.years}</output>
+      </label>
+      <label>
+        Interest
+        <output class="dollar">{formatNumber($totals.interest)}</output>
+      </label>
+    </fieldset>
   </form>
 
   <hr />
@@ -108,13 +161,23 @@
 </main>
 
 <style lang="css">
+  .dollar::before {
+    content: "$";
+  }
+
+  .percent::before {
+    content: "%";
+  }
+
   form {
     width: fit-content;
   }
+
   label {
-    display: flex;
+    display: grid;
     justify-content: space-between;
     gap: 1em;
+    grid-template-columns: repeat(2, 1fr);
   }
 
   table {
